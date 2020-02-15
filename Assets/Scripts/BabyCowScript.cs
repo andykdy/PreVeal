6ea7e10eb95ede133905515056abe42a8ee435d5
Andy Kim <andykdy@students.cs.ubc.ca>
2020-02-15 03:46:55 -0800

    Checkpoint

--- Assets/Scripts/BabyCowScript.cs
+++ Assets/Scripts/BabyCowScript.cs
@@ -1,36 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BabyCowScript : MonoBehaviour
 {
-    public float speed = 10f;
-    public float slowModifier = 5f;
-    public int score = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed = 10f;
+	public float slowModifier = 5f;
+	public int score = 1;
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // for testing
-        if (Input.GetKey(KeyCode.Space)) {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.up * slowModifier);
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		// for testing
+		if (Input.GetKey(KeyCode.Space)) {
+			gameObject.GetComponent<Rigidbody2D>().AddForce(-transform.up * slowModifier);
+		}
+		Vector2 movement = Vector2.zero;
+		if (Input.GetKey(KeyCode.W)) {
+			movement += Vector2.up;
+		} 
+		if (Input.GetKey(KeyCode.A)) {
+			movement += Vector2.left;
+		} 
+		if (Input.GetKey(KeyCode.S)) {
+			movement += Vector2.down;        
+		} 
+		if (Input.GetKey(KeyCode.D)) {
+			movement += Vector2.right;
+		}
+		movement.Normalize();
+		transform.Translate(movement * speed * Time.deltaTime);
+		if (movement.x * movement.y != 0)
+		{
+			if (movement.y < 0)
+				gameObject.transform.GetChild(0).rotation = Quaternion.Euler(0,0, 180 * (Mathf.PI -Mathf.Atan(movement.y / movement.x))/Mathf.PI); 
+			else 
+				gameObject.transform.GetChild(0).rotation = Quaternion.Euler(0, 0, -180 * Mathf.Atan(movement.y / movement.x)/Mathf.PI);
+		}
+		else 
+			gameObject.transform.GetChild(0).rotation = Quaternion.identity;
 
-        if (Input.GetKey(KeyCode.W)) {
-            transform.Translate (Vector2.up * speed * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.A)) {
-            transform.Translate (Vector2.left * speed * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.S)) {
-            transform.Translate (Vector2.down * speed * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.D)) {
-            transform.Translate (Vector2.right * speed * Time.deltaTime);
-        }
-
+		//transform.Rotate(new Vector3(0,0,1), Mathf.Atan(movement.y/movement.x));
 		// main camera
 		var dist = (transform.position - Camera.main.transform.position).z;
 
@@ -43,27 +59,27 @@ public class BabyCowScript : MonoBehaviour
 			Mathf.Clamp(transform.position.x, left, right),
 			Mathf.Clamp(transform.position.y, top, bottom),
 			transform.position.z);
-    }
+	}
 
-    void onTriggerEnter2D(Collider2D col) {
-        // cows
-        if (col.tag == "cow") {
-            // TODO solve the Cow Packing Problem
-            // we should increment the score when we add a cow
-            // with a maximum threshold
-        }
+	void onTriggerEnter2D(Collider2D col) {
+		// cows
+		if (col.tag == "cow") {
+			// TODO solve the Cow Packing Problem
+			// we should increment the score when we add a cow
+			// with a maximum threshold
+		}
 
-        if (col.tag == "potato") {
-            score--;
-            if (score == 0) {
-                Destroy(gameObject);
-            }
-        }
+		if (col.tag == "potato") {
+			score--;
+			if (score == 0) {
+				Destroy(gameObject);
+			}
+		}
 
-        if (col.tag == "mud") {
-            // gameObject.AddComponent<Rigidbody2D>().addForce(transform.down * slowModifier);
-        }
+		if (col.tag == "mud") {
+			// gameObject.AddComponent<Rigidbody2D>().addForce(transform.down * slowModifier);
+		}
 
-        // TODO show game over when cow dies
-    }
+		// TODO show game over when cow dies
+	}
 }
---
+++ Assets/Scripts/FollowerCow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using UnityEditor;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class FollowerCow : MonoBehaviour
+{
+    enum CowState { Idle, Following}
+
+    public float flock_range;
+    public float avoid_range;
+    public float speed;
+    public GameObject player_prefab;
+    private CowState cur_state;
+    private Rigidbody2D rgbd;
+    private static float tolerance = 0.3f;
+    private static float rot_speed = 0.25f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cur_state = CowState.Idle;
+        rgbd = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cur_state == CowState.Idle)
+        {
+            rgbd.transform.Translate(Vector3.down * speed *Time.deltaTime);
+            if (Vector2.Distance(player_prefab.transform.position, gameObject.transform.position) < 2)
+            {
+                cur_state = CowState.Following;
+            }
+        }
+
+        if (cur_state == CowState.Following)
+        {
+            FlockRoutine(GameObject.FindGameObjectsWithTag("cow"));
+        }
+    }
+    
+    private void FlockRoutine(GameObject[] cows)
+    {
+        List<Vector3> nearby_pos = new List<Vector3>();
+        List<float> nearby_rot = new List<float>();
+        for (int i = 0; i < cows.Length; i++)
+        {
+            if (Vector2.Distance(cows[i].transform.position, gameObject.transform.position) < flock_range)
+            {
+                nearby_pos.Add(cows[i].transform.position);
+                nearby_rot.Add(cows[i].transform.eulerAngles.z);
+            }
+        }
+        Debug.Log(nearby_rot.Average()-180);
+        gameObject.transform.rotation = Quaternion.RotateTowards(rgbd.transform.rotation, Quaternion.Euler(0, 0, nearby_rot.Average()), 0.25f);
+    }
+}
+
